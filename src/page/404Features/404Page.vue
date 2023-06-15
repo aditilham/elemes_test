8539314e88e3a463626642355044648c1c93826f
adit.it <adit.it@langitpayment.com>
2023-06-15 20:00:29 +0700

    add coming soon and error 404 page

--- src/page/404Features/404Page.vue
+++ src/page/404Features/404Page.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    Page not found
-
+  <div class="flex flex-row min-h-screen justify-center items-center">
+      <div class="flex flex-col items-center">
+           <div class="w-6/12 mx-auto">
+              <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_suhe7qtm.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>
+          </div>
+          <div class="px-2">
+              <p class="text-dark text-4xl font-bold text-center">Oops, This is not the webpage you are looking for</p>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "404Page"
+  name: 'ErrorNotFound',
 }
 </script>
 
 <style>
+
 </style>
